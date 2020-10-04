43ff58c30a7fa7e7e3f82100e202cad8107b6336
Haidang124 <haidangluu94@gmail.com>
2020-10-05 03:00:44 +0700

    edit temple

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
+// import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Login from './components/Login';
+import AdminLayout from './layouts/Admin.js';
+import AuthLayout from './layouts/Auth.js';
+
 const App: React.FC = () => {
-  return <div><span>Hello Body</span></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+          <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
+          <Route path="/login" render={(props) => <Login />} />
+          <Redirect from="/" to="/admin/index" />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
