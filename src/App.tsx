7c03203a72b37b7b462c96417957304fad1a8224
Haidang124 <haidangluu94@gmail.com>
2020-10-13 02:36:28 +0700

    connect BE

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import AdminLayout from './layouts/Admin';
 import AuthLayout from './layouts/Auth';
+import { userService } from './services/user/api';
+
 const App: React.FC = () => {
+  useEffect(() => {
+    userService.getUser().then((response) => {
+      console.log(response.data);
+    });
+  }, [])
   return (
     <div>
       <BrowserRouter>
